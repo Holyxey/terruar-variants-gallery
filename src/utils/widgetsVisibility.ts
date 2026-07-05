de3d36d151933b89eeda59b8c05ff2918d436372
Holyxey <text.holyxey@gmail.com>
2026-07-05 23:38:27 +0200

    feat(widgetsVisibility): via css

--- src/utils/widgetsVisibility.ts
+++ src/utils/widgetsVisibility.ts
@@ -1,40 +1,36 @@
-export function hideElement(el: HTMLElement) {
-  el.style.opacity = '0';
-  el.style.pointerEvents = 'none';
-  el.inert = true;
-}
-export function showElement(el: HTMLElement) {
-  el.style.opacity = '1';
-  el.style.pointerEvents = 'auto';
-  el.inert = false;
-}
-
-let timeout: ReturnType<typeof setInterval>;
+const attrsToHide = [
+  '[id^="jivo"]',
+  '[id^="znms"]',
+  'jdiv',
+  '[id*="alltouch"]',
+];
+const classAttr = 'hidden-widget';
+const styleId = 'widgetsVisibility';
 
 export function widgetsVisibility(to: 'hide' | 'show') {
   try {
-    const els = [
-      ...Array.from(document.querySelectorAll('[id^="jivo"]')),
-      ...Array.from(document.querySelectorAll('[id^="znms"]')),
-      document.querySelector('jdiv'),
-      document.querySelector('[id*="alltouch"]'),
-    ].filter((el): el is HTMLElement => el !== null);
-
-    if (timeout) clearInterval(timeout);
-
     switch (to) {
       case 'hide': {
-        timeout = setInterval(() => {
-          els.forEach((el) => el && hideElement(el));
-        }, 1000);
+        document.body.classList.add(classAttr);
         break;
       }
       case 'show': {
-        els.forEach((el) => el && showElement(el));
-        break;
+        document.body.classList.remove(classAttr);
       }
     }
   } catch (error) {
-    console.warn('', error);
+    console.warn('widgetsVisibility', error);
   }
+}
+
+if (
+  typeof document !== 'undefined' &&
+  !document.querySelector(`style[id="${styleId}"]`)
+) {
+  const style = document.createElement('style');
+  style.id = styleId;
+  style.textContent =
+    attrsToHide.map((attr) => `.${classAttr} ${attr}`).join(',') +
+    `{opacity: 0 !important; pointer-events: none !important;}`;
+  document.head.appendChild(style);
 }
