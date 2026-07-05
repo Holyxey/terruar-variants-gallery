3e73a41b9420c7828efa04cabb6dd9b262d1ec20
Holyxey <text.holyxey@gmail.com>
2026-07-05 23:16:08 +0200

    feat(widgetsVisibility): timeout

--- src/utils/widgetsVisibility.ts
+++ src/utils/widgetsVisibility.ts
@@ -1,23 +1,32 @@
 export function hideElement(el: HTMLElement) {
   el.style.opacity = '0';
   el.style.pointerEvents = 'none';
+  el.inert = true;
 }
 export function showElement(el: HTMLElement) {
   el.style.opacity = '1';
   el.style.pointerEvents = 'auto';
+  el.inert = false;
 }
+
+let timeout: ReturnType<typeof setInterval>;
 
 export function widgetsVisibility(to: 'hide' | 'show') {
   try {
-    const jdiv: HTMLElement | null = document.querySelector('jdiv');
-    const ctouch: HTMLElement | null =
-      document.querySelector('[id*="alltouch"]');
+    const els = [
+      ...Array.from(document.querySelectorAll('[id^="jivo"]')),
+      ...Array.from(document.querySelectorAll('[id^="znms"]')),
+      document.querySelector('jdiv'),
+      document.querySelector('[id*="alltouch"]'),
+    ].filter((el): el is HTMLElement => el !== null);
 
-    const els = [jdiv, ctouch];
+    if (timeout) clearInterval(timeout);
 
     switch (to) {
       case 'hide': {
-        els.forEach((el) => el && hideElement(el));
+        timeout = setInterval(() => {
+          els.forEach((el) => el && hideElement(el));
+        }, 1000);
         break;
       }
       case 'show': {
